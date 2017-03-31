218b23971bfa137152fdcf30a8d0de1897b20fdc
Sean Cross <sean@xobs.io>
2017-03-31 05:48:54 -0400

    wip: productizing tests
    Signed-off-by: Sean Cross <sean@xobs.io>

--- bin/measure-reset.c
+++ bin/measure-reset.c
@@ -12,142 +12,169 @@
    storing the result in RESULT.
    Return 1 if the difference is negative, otherwise 0.  */
 
-int timeval_subtract (struct timeval *result,
-                      struct timeval *x, struct timeval *y)
+static int timeval_subtract(struct timeval *result,
+                            struct timeval *x, struct timeval *y)
 {
-   /* Perform the carry for the later subtraction by updating y. */
-   if (x->tv_usec < y->tv_usec) {
-     int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-     y->tv_usec -= 1000000 * nsec;
-     y->tv_sec += nsec;
-   }
-   if (x->tv_usec - y->tv_usec > 1000000) {
-     int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-     y->tv_usec += 1000000 * nsec;
-     y->tv_sec -= nsec;
-   }
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_usec < y->tv_usec) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    y->tv_usec -= 1000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_usec - y->tv_usec > 1000000) {
+    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+    y->tv_usec += 1000000 * nsec;
+    y->tv_sec -= nsec;
+  }
 
-   /* Compute the time remaining to wait.
-      tv_usec is certainly positive. */
-   result->tv_sec = x->tv_sec - y->tv_sec;
-   result->tv_usec = x->tv_usec - y->tv_usec;
+  /* Compute the time remaining to wait.
+     tv_usec is certainly positive. */
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_usec = x->tv_usec - y->tv_usec;
 
-   /* Return 1 if result is negative. */
-   return x->tv_sec < y->tv_sec;
+  /* Return 1 if result is negative. */
+  return x->tv_sec < y->tv_sec;
 }
 
 enum edge {
-	RISING,
-	FALLING,
-	BOTH,
-	NONE,
+  RISING,
+  FALLING,
+  BOTH,
+  NONE,
 };
 
 static int write_file(const char *filename, const char *string) {
-	int fd = open(filename, O_WRONLY);
-	if (fd == -1)
-		return fd;
+  int fd = open(filename, O_WRONLY);
+  if (fd == -1)
+    return fd;
 
-	write(fd, string, strlen(string));
-	close(fd);
-	return 0;
+  write(fd, string, strlen(string));
+  close(fd);
+  return 0;
 }
 
 static int export_gpio(int gpio) {
-	char filename[512];
-	char string[128];
+  char filename[512];
+  char string[128];
 
-	snprintf(filename, sizeof(filename)-1, "/sys/class/gpio/export");
-	snprintf(string, sizeof(string)-1, "%d", gpio);
-	if (write_file(filename, string))
-		return -1;
+  snprintf(filename, sizeof(filename)-1, "/sys/class/gpio/export");
+  snprintf(string, sizeof(string)-1, "%d", gpio);
+  if (write_file(filename, string))
+    return -1;
 
-	snprintf(filename, sizeof(filename)-1, "/sys/class/gpio/gpio%d/direction", gpio);
-	snprintf(string, sizeof(string)-1, "in");
-	if (write_file(filename, string))
-		return -1;
+  snprintf(filename, sizeof(filename)-1, "/sys/class/gpio/gpio%d/direction", gpio);
+  snprintf(string, sizeof(string)-1, "in");
+  if (write_file(filename, string))
+    return -1;
 
-	return 0;
+  return 0;
 }
 
 static int set_edge(int gpio, enum edge edge) {
-	char filename[512];
-	const char *edge_str;
+  char filename[512];
+  const char *edge_str;
 
-	if (edge == RISING)
-		edge_str = "rising";
-	else if (edge == FALLING)
-		edge_str = "falling";
-	else if (edge == BOTH)
-		edge_str = "both";
-	else if (edge == NONE)
-		edge_str = "none";
-	else
-		return -1;
+  if (edge == RISING)
+    edge_str = "rising";
+  else if (edge == FALLING)
+    edge_str = "falling";
+  else if (edge == BOTH)
+    edge_str = "both";
+  else if (edge == NONE)
+    edge_str = "none";
+  else
+    return -1;
 
-	snprintf(filename, sizeof(filename)-1, "/sys/class/gpio/gpio%d/edge", gpio);
-	if (write_file(filename, edge_str))
-		return -1;
-	return 0;
+  snprintf(filename, sizeof(filename)-1, "/sys/class/gpio/gpio%d/edge", gpio);
+  if (write_file(filename, edge_str))
+    return -1;
+  return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
-   char str[256];
-   struct pollfd pfd;
-   int fd, gpio;
-   char buf[8];
+  char str[256];
+  struct pollfd pfd;
+  int fd;
+  int gpio = GPIO;
+  int min_pulse = 0;
+  int max_pulse = 10000;
+  char buf[8];
 
-   if (argc > 1)
-	   gpio = atoi(argv[1]);
-   else
-	   gpio = GPIO;
+  if (argc > 1) {
+    min_pulse = atoi(argv[1]);
+    if (argc > 2) {
+      max_pulse = atoi(argv[2]);
+      if (argc > 3) {
+        gpio = atoi(argv[3]);
+      }
+    }
+  }
 
-   if (export_gpio(gpio)) {
-	   perror("Unable to export GPIO");
-	   return 1;
-   }
+  if (export_gpio(gpio)) {
+    perror("Unable to export GPIO");
+    return 1;
+  }
 
-   snprintf(str, sizeof(str)-1, "/sys/class/gpio/gpio%d/value", gpio);
+  snprintf(str, sizeof(str)-1, "/sys/class/gpio/gpio%d/value", gpio);
+  if ((fd = open(str, O_RDONLY)) < 0) {
+     fprintf(stderr, "Failed, gpio %d not exported.\n", gpio);
+     exit(1);
+  }
 
-   if ((fd = open(str, O_RDONLY)) < 0)
-   {
-      fprintf(stderr, "Failed, gpio %d not exported.\n", gpio);
-      exit(1);
-   }
+  printf("Press the reset button, and we'll measure the time.\n");
+  printf("The time must be more than %d ms and less than %d ms\n",
+      min_pulse, max_pulse);
+  fflush(stdout);
 
-   printf("Press the reset button, and we'll measure the time.\n");
-   fflush(stdout);
 
-   set_edge(gpio, FALLING);
+  // Wait for the reset pulse to fall, indicating the board is in reset.
+  set_edge(gpio, FALLING);
 
-   pfd.fd = fd;
+  pfd.fd = fd;
+  pfd.events = POLLPRI;
 
-   pfd.events = POLLPRI;
+  lseek(fd, 0, SEEK_SET);    /* consume any prior interrupt */
+  read(fd, buf, sizeof buf);
 
-   lseek(fd, 0, SEEK_SET);    /* consume any prior interrupt */
-   read(fd, buf, sizeof buf);
-
-   poll(&pfd, 1, -1);         /* wait for interrupt */
+  poll(&pfd, 1, -1);         /* wait for interrupt */
   
-   lseek(fd, 0, SEEK_SET);    /* consume interrupt */
-   read(fd, buf, sizeof buf);
+  lseek(fd, 0, SEEK_SET);    /* consume interrupt */
+  read(fd, buf, sizeof buf);
 
-   struct timeval starttime, endtime, difftime;
-   gettimeofday(&starttime, NULL);
-   set_edge(gpio, RISING);
+  struct timeval starttime, endtime, difftime;
+  gettimeofday(&starttime, NULL);
+  set_edge(gpio, RISING);
 
-   poll(&pfd, 1, -1);         /* wait for interrupt */
+  poll(&pfd, 1, -1);         /* wait for interrupt */
   
-   lseek(fd, 0, SEEK_SET);    /* consume interrupt */
-   read(fd, buf, sizeof buf);
+  lseek(fd, 0, SEEK_SET);    /* consume interrupt */
+  read(fd, buf, sizeof buf);
 
-   gettimeofday(&endtime, NULL);
+  gettimeofday(&endtime, NULL);
 
-   timeval_subtract(&difftime, &endtime, &starttime);
-   printf("Reset pulse is %ld.%06ld long\n", difftime.tv_sec, difftime.tv_usec);
-   fflush(stdout);
+  timeval_subtract(&difftime, &endtime, &starttime);
+  printf("Reset pulse is %ld.%06ld long\n", difftime.tv_sec, difftime.tv_usec);
 
-   exit(0);
+  if (difftime.tv_sec) {
+    printf("Pulse way too long\n");
+    fflush(stdout);
+    return 1;
+  }
+
+  if (difftime.tv_usec < min_pulse) {
+    printf("Pulse too short.\n");
+    fflush(stdout);
+    return 1;
+  }
+  
+  if (difftime.tv_usec > max_pulse) {
+    printf("Pulse too long.\n");
+    fflush(stdout);
+    return 1;
+  }
+
+  printf("Pulse is acceptable.\n");
+  return 0;
 }

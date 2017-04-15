18956ff7f046be2abcaa782984956ff49825df44
Sean Cross <sean@xobs.io>
2017-04-15 03:06:11 -0400

    leds: make error messages slightly clearer
    Signed-off-by: Sean Cross <sean@xobs.io>

--- bin/05-led-test.sh
+++ bin/05-led-test.sh
@@ -17,15 +17,18 @@ echo -n 'l' > ${uart}
 grep -q "PWM LED test" ${uart}
 
 error_msg=""
+failure_pins=""
 for pin in $(seq 0 5)
 do
 	echo "    Pin A${pin}"
 	echo -n ${pin} > ${uart}
 	if ! pulse_count ${pin} 256
 	then
-		echo "        Pulse out of range: ${range_val}"
 		error_msg="${error_msg}  ${pin}:${range_diff}"
 		error_count=$((${error_count} + 1))
+		failure_pins="${failure_pins} ${pin}"
+#		echo "        Pulse out of range: ${range_val}"
+		echo "LED: ${failure_pins}"
 #	else
 #		echo "        Pulse is in range: ${range_val}"
 	fi
@@ -34,7 +37,7 @@ echo -n q > ${uart}
 
 if ! [ -z "${error_msg}" ]
 then
-	echo "LED: ${error_msg}"
+	echo "LED: ${failure_pins}"
 	exit ${error_count}
 fi
 
--- bin/06-rgb-test.sh
+++ bin/06-rgb-test.sh
@@ -11,16 +11,20 @@ echo w > ${uart}
 grep -q "RGB LED test" ${uart}
 
 error_msg=""
+error_pins=""
 for color in r g b REx GEx BEx
 do
 	# Send the color name to the UART, selecting it.
-	echo ${color} | cut -c 1 > ${uart}
+	color_abbr=$(echo ${color} | cut -c 1)
+	echo ${color_abbr} > ${uart}
 	echo "    ${color}"
 	if ! pulse_count rgb 128
 	then
-		echo "        Pulse out of range: ${range_val}"
 		error_msg="${error_msg} ${color}:${range_diff}"
+		error_pins="${error_pins} ${color_abbr}"
 		error_count=$((${error_count} + 1))
+#		echo "        Pulse out of range: ${range_val}"
+		echo "RGB: ${error_pins}"
 #	else
 #		echo "        Pulse is in range: ${range_val}"
 	fi
@@ -28,7 +32,7 @@ done
 
 if ! [ -z "${error_msg}" ]
 then
-	echo "RGB: ${error_msg}"
+	echo "RGB: ${error_pins}"
 	exit ${error_count}
 fi
 

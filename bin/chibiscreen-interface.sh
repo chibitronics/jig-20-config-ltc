e8907a71a6563bb613c550cc82131221bff6c7b2
Sean Cross <sean@xobs.io>
2017-04-15 02:36:02 -0400

    chibiscreen-interface: fix echo bug
    We were inadvertantly echoing values back to the controller.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- bin/chibiscreen-interface.sh
+++ bin/chibiscreen-interface.sh
@@ -7,17 +7,17 @@ stty -F ${uart} ${baud} -icrnl -imaxbel -opost -onlcr -isig -icanon -echo
 echo "HELLO bash-chibiscreen-logger 1.0"
 while read line
 do
-	if echo "${line}" | grep -i '^start'
+	if echo "${line}" | grep -iq '^start'
 	then
 		# On start, issue a 'SYN' to clear the screen
 		echo '#SYN' > ${uart}
 		echo 'Running...' > ${uart}
-	elif echo "${line}" | grep -i '^hello'
+	elif echo "${line}" | grep -iq '^hello'
 	then
 		echo '#SYN' > ${uart}
 		echo -n "Jig: " > ${uart}
 		echo "${line}" | awk '{ sub(/([^ ]+ +){1}/,"") }1' > ${uart}
-	elif echo "${line}" | grep -i '^fail'
+	elif echo "${line}" | grep -iq '^fail'
 	then
 		# awk command from http://stackoverflow.com/questions/2626274/print-all-but-the-first-three-columns
 		echo "${line}" | awk '{ sub(/([^ ]+ +){2}/,"") }1' > ${uart}
@@ -27,8 +27,6 @@ do
                 if [ ${result} -ge 200 -a ${result} -lt 300 ]
                 then
                         echo 'Pass' > ${uart}
-#                else
-#                        red_on
                 fi
 
 	elif echo "${line}" | grep -iq '^exit'

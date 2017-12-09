fcf355f13449865d39194d6ee7a41b485bf4f2e2
Sean Cross <sean@xobs.io>
2017-12-09 16:18:48 +0800

    rgb-test: print out measured rate on error and retry
    When the RGB LED fails, print the measured pulse count.  This will
    assist in debugging.
    
    Additionally, try the test twice, in case it fails the first time.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- bin/06-rgb-test.sh
+++ bin/06-rgb-test.sh
@@ -18,10 +18,10 @@ do
 	color_abbr=$(echo ${color} | cut -c 1)
 	echo ${color_abbr} > ${uart}
 	echo "    ${color}"
-	if ! pulse_count rgb
+	if ! pulse_count rgb && ! pulse_count rgb
 	then
 		error_msg="${error_msg} ${color}:${range_diff}"
-		error_pins="${error_pins} ${color_abbr}"
+		error_pins="${error_pins} ${color_abbr}:${range_diff}"
 		error_count=$((${error_count} + 1))
 #		echo "        Pulse out of range: ${range_val}"
 		echo "RGB: ${error_pins}"
